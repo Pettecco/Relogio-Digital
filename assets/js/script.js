4502cc423a6e867f710fc9ad57232e51ffc68aab
Petterson Sousa <pettersonibs@gmail.com>
2023-02-20 15:15:43 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
-    let sec = dateToday.getSeconds();
+    let secs = dateToday.getSeconds();
 
     if(hr < 10) hr = '0' + hr;
     if(min < 10) min = '0' + min;
-    if(sec < 10) sec = '0' + sec;
+    if(secs < 10) secs = '0' + secs;
 
     horas.textContent = hr;
     minutos.textContent = min;
-    segundos.textContent = sec;
+    segundos.textContent = secs;
 })
